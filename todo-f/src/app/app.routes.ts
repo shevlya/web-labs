2c362d26333dfc88fdcf69745fa3452a04846f21
Dasha Shevlya <shevliakova.d@gmail.com>
2026-03-25 19:55:24 +0400

    ich habe die dateien erstellt

--- todo-f/src/app/app.routes.ts
+++ todo-f/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'tasks', component: TaskListComponent},
+    {path: 'task/new', component: EditTaskComponent},
+    {path: 'tasks/:taskId', component: EditTaskComponent},
+    {path: '', redirectTo: '/tasks', pathMatch: 'full'},
+    {path: '**', redirectTo: '/tasks'}
+];
