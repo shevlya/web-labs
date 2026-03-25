97068bdaa29ee55bded0321c780e3d1873fefb4f
Dasha Shevlya <shevliakova.d@gmail.com>
2026-03-25 21:06:01 +0400

    ich habe interface hinzugefuegt

--- todo-f/src/app/models/auth.ts
+++ todo-f/src/app/models/auth.ts
@@ -1,14 +1,14 @@
 export interface LoginRequest {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export interface TokenResponse {
-    accessToken: string;
-    refreshToken: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
 export interface UserInfo {
-    username: string;
-    roles: string[];
+  username: string;
+  roles: string[];
 }
